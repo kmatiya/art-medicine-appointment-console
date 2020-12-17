0dc807cf4ce32c055fc3a5b577c91f0acc6db24f
kmatiya <khormatiya>
2020-12-17 14:08:54 +0200

    Add CTX 480

---
+++ src/main/java/api/regimen/medicine/art/CTX480.java
@@ -0,0 +1,38 @@
+package api.regimen.medicine.art;
+
+import api.regimen.medicine.MedicineService;
+import api.regimen.weight.WeightRangeMapper;
+import api.util.DosageAndWeightMapper;
+import model.DosageAndWeight;
+import model.medicineType.MedicineType;
+
+import java.util.HashMap;
+
+public class CTX480 implements MedicineService {
+
+    private final HashMap<String, DosageAndWeight> medicineDosage;
+
+    public CTX480()
+    {
+        medicineDosage = new HashMap<>();
+
+        medicineDosage.put(WeightRangeMapper.LESS_THAN_TEN, DosageAndWeightMapper.setDosageAndWeightOfMedicine(6,9.9,0,0.5,MedicineType.TABLET));
+        medicineDosage.put(WeightRangeMapper.LESS_THAN_FOURTEEN, DosageAndWeightMapper.setDosageAndWeightOfMedicine(10,13.9,0,0.5,MedicineType.TABLET));
+        medicineDosage.put(WeightRangeMapper.LESS_THAN_TWENTY, DosageAndWeightMapper.setDosageAndWeightOfMedicine(14,19.9,0,1,MedicineType.TABLET));
+        medicineDosage.put(WeightRangeMapper.LESS_THAN_TWENTY_FIVE, DosageAndWeightMapper.setDosageAndWeightOfMedicine(20,24.9,0,1,MedicineType.TABLET));
+        medicineDosage.put(WeightRangeMapper.LESS_THAN_THIRTY, DosageAndWeightMapper.setDosageAndWeightOfMedicine(25,29.9,0,2,MedicineType.TABLET));
+        medicineDosage.put(WeightRangeMapper.LESS_THAN_THIRTY_FIVE, DosageAndWeightMapper.setDosageAndWeightOfMedicine(30,34.9,0,2,MedicineType.TABLET));
+        medicineDosage.put(WeightRangeMapper.LESS_THAN_FORTY, DosageAndWeightMapper.setDosageAndWeightOfMedicine(35,39.9,0,2,MedicineType.TABLET));
+        medicineDosage.put(WeightRangeMapper.ABOVE_FORTY, DosageAndWeightMapper.setDosageAndWeightOfMedicine(40,300,0,2,MedicineType.TABLET));
+    }
+
+    @Override
+    public HashMap<String,DosageAndWeight> getDosageAndWeightRange() {
+        return medicineDosage;
+    }
+
+    @Override
+    public String getName() {
+        return "LPV/r pellets (in caps)";
+    }
+}
