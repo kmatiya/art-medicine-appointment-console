0f8721969d1c8fc6f87a828811f45fce39c56775
kmatiya <khormatiya@gmail.com>
2020-12-17 13:15:06 +0200

    Add AZT/3TC and AZT/3TC/NVP

---
+++ src/main/java/api/regimen/medicine/art/AZT3TCNVP.java
@@ -0,0 +1,43 @@
+package api.regimen.medicine.art;
+
+import api.regimen.medicine.MedicineService;
+import api.regimen.weight.WeightRangeMapper;
+import api.util.DosageAndWeightMapper;
+import model.DosageAndWeight;
+import model.medicineType.MedicineType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class AZT3TCNVP implements MedicineService {
+
+    private final HashMap<String, DosageAndWeight> medicineDosage;
+
+    public AZT3TCNVP()
+    {
+        medicineDosage = new HashMap<>();
+        List<DosageAndWeight> dosageAndWeightList = new ArrayList<>();
+
+        medicineDosage.put(WeightRangeMapper.LESS_THAN_FOUR, DosageAndWeightMapper.setDosageAndWeightOfMedicine(3,3.9,1,1, MedicineType.TABLET));
+        medicineDosage.put(WeightRangeMapper.LESS_THAN_SIX, DosageAndWeightMapper.setDosageAndWeightOfMedicine(4,5.9,1,1,MedicineType.TABLET));
+        medicineDosage.put(WeightRangeMapper.LESS_THAN_TEN, DosageAndWeightMapper.setDosageAndWeightOfMedicine(6,9.9,1.5,1.5,MedicineType.TABLET));
+        medicineDosage.put(WeightRangeMapper.LESS_THAN_FOURTEEN, DosageAndWeightMapper.setDosageAndWeightOfMedicine(10,13.9,2,2,MedicineType.TABLET));
+        medicineDosage.put(WeightRangeMapper.LESS_THAN_TWENTY, DosageAndWeightMapper.setDosageAndWeightOfMedicine(14,19.9,2.5,2.5,MedicineType.TABLET));
+        medicineDosage.put(WeightRangeMapper.LESS_THAN_TWENTY_FIVE, DosageAndWeightMapper.setDosageAndWeightOfMedicine(20,24.9,3,3,MedicineType.TABLET));
+        medicineDosage.put(WeightRangeMapper.LESS_THAN_THIRTY, DosageAndWeightMapper.setDosageAndWeightOfMedicine(25,29.9,1,1,MedicineType.TABLET));
+        medicineDosage.put(WeightRangeMapper.LESS_THAN_THIRTY_FIVE, DosageAndWeightMapper.setDosageAndWeightOfMedicine(30,34.9,1,1,MedicineType.TABLET));
+        medicineDosage.put(WeightRangeMapper.LESS_THAN_FORTY, DosageAndWeightMapper.setDosageAndWeightOfMedicine(35,39.9,1,1,MedicineType.TABLET));
+        medicineDosage.put(WeightRangeMapper.ABOVE_FORTY, DosageAndWeightMapper.setDosageAndWeightOfMedicine(40,300,1,1,MedicineType.TABLET));
+    }
+
+    @Override
+    public HashMap<String,DosageAndWeight> getDosageAndWeightRange() {
+        return medicineDosage;
+    }
+
+    @Override
+    public String getName() {
+        return "NVP";
+    }
+}
