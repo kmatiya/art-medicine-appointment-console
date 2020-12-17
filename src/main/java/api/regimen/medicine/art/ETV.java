884c55b474d6cfb9b86c96b607f7203424cdea95
kmatiya <khormatiya>
2020-12-17 14:00:44 +0200

    Add ETV

---
+++ src/main/java/api/regimen/medicine/art/ETV.java
@@ -0,0 +1,32 @@
+package api.regimen.medicine.art;
+
+import api.regimen.medicine.MedicineService;
+import api.regimen.weight.WeightRangeMapper;
+import api.util.DosageAndWeightMapper;
+import model.DosageAndWeight;
+import model.medicineType.MedicineType;
+
+import java.util.HashMap;
+
+public class ETV implements MedicineService {
+
+    private final HashMap<String, DosageAndWeight> medicineDosage;
+
+    public ETV()
+    {
+        medicineDosage = new HashMap<>();
+
+        medicineDosage.put(WeightRangeMapper.LESS_THAN_FORTY, DosageAndWeightMapper.setDosageAndWeightOfMedicine(35,39.9,2,2,MedicineType.TABLET));
+        medicineDosage.put(WeightRangeMapper.ABOVE_FORTY, DosageAndWeightMapper.setDosageAndWeightOfMedicine(40,300,2,2,MedicineType.TABLET));
+    }
+
+    @Override
+    public HashMap<String,DosageAndWeight> getDosageAndWeightRange() {
+        return medicineDosage;
+    }
+
+    @Override
+    public String getName() {
+        return "DRV";
+    }
+}
