b88dc4bdb2ce011911c100823a51dc127fd7f91d
kmatiya <khormatiya>
2020-12-17 14:29:25 +0200

    Add Medicine Name MetaData

---
+++ src/main/java/metadata/MedicineNameMetaData.java
@@ -0,0 +1,24 @@
+package metadata;
+
+public class MedicineNameMetaData {
+    public static String NVP = "NVP";
+    public static String AZT_3TC = "AZT / 3TC";
+    public static String AZT_3TC_NVP = "AZT / 3TC / NVP";
+    public static String LPV_r_LIQUID_OR_TABLETS = "LPV/r liquid/tabs";
+    public static String LPV_r_PELLETS_IN_CAPS = "LPV/r Pellets (in caps)";
+    public static String EFV = "EFV";
+    public static String ATV_r = "ATV/r";
+    public static String TDF_3TC = "TDF / 3TC";
+    public static String TDF_3TC_EFV = "TDF / 3TC / EFV";
+    public static String TDF_3TC_DTG = "TDF / 3TC / DTG";
+    public static String DTG = "DTG";
+    public static String DRV = "DRV";
+    public static String R = "R";
+    public static String ETV = "ETV";
+    public static String RAL = "RAL";
+    public static String CTX_120 = "CTX 120";
+    public static String INH_100 = "INH 100";
+    public static String CTX_480 = "CTX 480";
+    public static String CTX_960 = "CTX 960";
+    public static String INH_300 = "INH 300";
+}
