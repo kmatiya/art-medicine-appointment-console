258890fb4cd1a666ebf73aaccf12cd7f90ffa54a
kmatiya <khormatiya@gmail.com>
2020-12-18 10:28:06 +0200

    Add getWeightRange based on weight given

--- src/main/java/api/medicine/art/regimen/validator/impl/RegimenValidatorImpl.java
+++ src/main/java/api/medicine/art/regimen/validator/impl/RegimenValidatorImpl.java
@@ -2,6 +2,8 @@ package api.medicine.art.regimen.validator.impl;
 
 import api.medicine.MedicineService;
 import api.medicine.art.regimen.validator.RegimenValidator;
+import api.medicine.weight.WeightRangeMapper;
+import metadata.RegimenNameMetaData;
 
 import java.util.ArrayList;
 
@@ -9,11 +11,72 @@ public class RegimenValidatorImpl implements RegimenValidator {
 
     @Override
     public boolean isRegimenNameValid(String regimen) {
+        if(regimen.equals(RegimenNameMetaData.REGIMEN_0A)){
+            return true;
+        }
+        if(regimen.equals(RegimenNameMetaData.REGIMEN_0P)){
+            return true;
+        }
+        if(regimen.equals(RegimenNameMetaData.REGIMEN_2A)){
+            return true;
+        }
+        if(regimen.equals(RegimenNameMetaData.REGIMEN_2P)){
+            return true;
+        }
+        if(regimen.equals(RegimenNameMetaData.REGIMEN_4A)){
+            return true;
+        }
+        if(regimen.equals(RegimenNameMetaData.REGIMEN_4P)){
+            return true;
+        }
+        if(regimen.equals(RegimenNameMetaData.REGIMEN_5A)){
+            return true;
+        }
+        if(regimen.equals(RegimenNameMetaData.REGIMEN_6A)){
+            return true;
+        }
+        if(regimen.equals(RegimenNameMetaData.REGIMEN_7A)){
+            return true;
+        }
+        if(regimen.equals(RegimenNameMetaData.REGIMEN_8A)){
+            return true;
+        }
+        if(regimen.equals(RegimenNameMetaData.REGIMEN_9A)){
+            return true;
+        }
+        if(regimen.equals(RegimenNameMetaData.REGIMEN_9P)){
+            return true;
+        }
+        if(regimen.equals(RegimenNameMetaData.REGIMEN_10A)){
+            return true;
+        }
+        if(regimen.equals(RegimenNameMetaData.REGIMEN_11A)){
+            return true;
+        }
+        if(regimen.equals(RegimenNameMetaData.REGIMEN_11P)){
+            return true;
+        }
+        if(regimen.equals(RegimenNameMetaData.REGIMEN_12A)){
+            return true;
+        }
+        if(regimen.equals(RegimenNameMetaData.REGIMEN_13A)){
+            return true;
+        }
+        if(regimen.equals(RegimenNameMetaData.REGIMEN_14A)){
+            return true;
+        }
+        if(regimen.equals(RegimenNameMetaData.REGIMEN_15A)){
+            return true;
+        }
         return false;
     }
 
     @Override
-    public boolean isWeightValid(double weight, ArrayList<MedicineService> drugCombination) {
-        return false;
+    public String isWeightValid(double weight, ArrayList<MedicineService> drugCombination) {
+        String getWeightRangeOfPatient = WeightRangeMapper.getWeightRange(weight);
+        if(getWeightRangeOfPatient == null){
+            return "Weight is out of range, weight should be in the range of 3 to above 40 Kg";
+        }
+        return getWeightRangeOfPatient;
     }
 }
